5092bd96fd9143517defb0b46a1e9f6d8b830e1b
Dılo sürücü <berxudar@gmail.com>
2022-10-25 11:13:42 +0300

    Use keyword included for hash

--- src/ClosureHash.php
+++ src/ClosureHash.php
@@ -12,6 +12,11 @@ use ReflectionFunction;
 class ClosureHash implements HashInterface
 {
     /**
+     * @var ReflectionFunction
+     */
+    private ReflectionFunction $reflection;
+
+    /**
      * @throws ReflectionException
      */
     public function __construct(Closure $closure)
@@ -20,22 +25,65 @@ class ClosureHash implements HashInterface
     }
 
 
+    /**
+     * @return string
+     */
     public function getHash(): string
     {
         return md5(
             sprintf(
-                '%s.%d',
+                '%s.%d.%s',
                 $this->getFileName(),
-                $this->getEndLine()
+                $this->getEndLine(),
+                $this->useVariablesToString()
             )
         );
     }
 
+    /**
+     * @return string
+     */
+    private function useVariablesToString(): string
+    {
+        $string = (string)null;
+        foreach ($this->getUseVariables() as $useVariable) {
+            if (is_object($useVariable)) {
+                $string .= $this->objectToString($useVariable);
+                continue;
+            }
+            $string .= serialize($useVariable);
+        }
+        return $string;
+    }
+
+    /**
+     * @param object $object
+     * @return string
+     */
+    private function objectToString(object $object): string
+    {
+        return spl_object_hash($object);
+    }
+
+    /**
+     * @return array
+     */
+    private function getUseVariables(): array
+    {
+        return $this->reflection->getClosureUsedVariables();
+    }
+
+    /**
+     * @return int
+     */
     private function getEndLine(): int
     {
         return $this->reflection->getEndLine();
     }
 
+    /**
+     * @return string
+     */
     private function getFileName(): string
     {
         return $this->reflection->getFileName();
--- tests/phpunit/MemoizeTest.php
+++ tests/phpunit/MemoizeTest.php
@@ -8,9 +8,12 @@ use function Zeus\Memoize\once;
 class MemoizeTest extends TestCase
 {
     /**
+     * @test
+     * @return void
      * @throws ReflectionException
+     *
      */
-    public function testOnce(): void
+    public function mainOnce(): void
     {
         /**
          * @throws Exception
@@ -20,8 +23,65 @@ class MemoizeTest extends TestCase
         };
 
         $first = once($closure);
-        foreach (range(1, 100) as $item) {
+        foreach (range(1, 10) as $item) {
             $this->assertEquals($first, once($closure));
         }
+    }
+
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function useVariables(): void
+    {
+        $anonymous = new class () {
+            public string $name = 'test';
+        };
+
+        $array = ['name' => 'ff'];
+        /**
+         * @throws Exception
+         */
+        $closure = static function () use ($anonymous, $array) {
+            return random_int(1, 100) . $anonymous->name . $array['name'];
+        };
+
+        $first = once($closure);
+        foreach (range(1, 10) as $item) {
+            $this->assertEquals($first, once($closure));
+        }
+    }
+
+
+    /**
+     * @test
+     * @return void
+     * @throws ReflectionException
+     */
+    public function handleUSeVariablesChanged(): void
+    {
+        $anonymous = new class () {
+            public string $name = 'test';
+        };
+
+        $array = ['name' => 'ff'];
+        /**
+         * @throws Exception
+         */
+        $closure = static function () use ($anonymous, &$array) {
+            return random_int(1, 100) . $anonymous->name . $array['name'];
+        };
+
+        $first = once($closure);
+        $array['name'] = 'changed';
+        $second = once($closure);
+        $this->assertNotEquals($second, $first);
+
+        $array['name'] = 'ff';
+        $third = once($closure);
+
+        $this->assertEquals($first, $third);
+        $this->assertNotEquals($second, $third);
     }
 }
